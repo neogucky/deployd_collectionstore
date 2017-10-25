f45eadcc12dc32699fdcb9a886e2697f28712168
Tim Rasim <rasim@imis.uni-luebeck.de>
2017-10-25 09:55:04 +0200

    Added params to cponnect to enable  capability

--- store.js
+++ store.js
@@ -31,9 +31,9 @@ DPDStore.prototype.setDeleteListener = function(listener){
 	this.deleteListener = listener;
 }
 
-DPDStore.prototype.connect = function(){
+DPDStore.prototype.connect = function( params ){
 	var self = this;
-	dpd[this.storeID].get(function(itemList, error) { //Use dpd.js to access the API
+	dpd[this.storeID].get(params, function(itemList, error) { //Use dpd.js to access the API
 
 		//FIXME: somehow this is triggered twice, second time with itemList = null. This workarround will not work with empty collections
 		if (itemList != null) {
@@ -66,29 +66,23 @@ DPDStore.prototype.afterInit = function(){
 		var self = this;
 
 	dpd[this.storeID].on('update', function(item) {
-		console.log('update: ' + self.storeID + ' ID: ' + item.id);
 		self.items[item.id] = item;
 		if (jQuery.isFunction(self.updateListener)){
-			console.log('informed update listener');
 			self.updateListener(item);
 		}	
 	});
 	
 	dpd[this.storeID].on('delete', function(item) {
-		console.log('delete: ' + self.storeID + ' ID: ' + item.id);
 		//FIXME: this is very resource hungry, since our arrays indexes are not in sequential order it has to do for now
 		self.items.splice($.inArray(item, self.items),1);
 		if (jQuery.isFunction(self.deleteListener)){
-			console.log('informed delete listener');
 			self.deleteListener(item);
 		}
 	});
 	
 	dpd[this.storeID].on('new', function(item) {
-		console.log('new: ' + self.storeID + ' ID: ' + item.id);
 		self.items[item.id] = item;
 		if (jQuery.isFunction(self.newListener)){
-			console.log('informed new listener');
 			self.newListener(item);
 		}
 	});
